241b1a94a53646d60ca49f1c5760eac26327f922
DevonSifford <devon.sifford@pcc.edu>
2019-06-03 17:34:16 -0700

    Added SFXController to Source folder and to Playing field
    Sounds housed in Resources folder

---
+++ DogTowerDefense/Assets/Source/SFXController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SFXController : MonoBehaviour
+{
+    public static AudioClip BulletSound, DoggoBarkingSound, DoggoHurtSound, EnemyDamageSound, PlacingSound, StartButtonSound;
+    static AudioSource audioSrc;
+
+    //TO CALL SOUND USE: SFXController.PlaySound("FileName");
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        BulletSound = Resources.Load<AudioClip>("Bullet");
+        DoggoBarkingSound = Resources.Load<AudioClip>("DoggoBarking");
+        DoggoHurtSound = Resources.Load<AudioClip>("DoggoHurt");
+        EnemyDamageSound = Resources.Load<AudioClip>("EnemyDamage");
+        PlacingSound = Resources.Load<AudioClip>("Placing");
+        StartButtonSound = Resources.Load<AudioClip>("StartButton");
+
+        audioSrc = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public static void PlaySound (string clip)
+    {
+        switch (clip)
+        {
+            case "Bullet":
+                audioSrc.PlayOneShot(BulletSound);
+                break;
+            case "DoggoBarking":
+                audioSrc.PlayOneShot(DoggoBarkingSound);
+                break;
+            case "DoggoHurt":
+                audioSrc.PlayOneShot(DoggoHurtSound);
+                break;
+            case "EnemyDamage":
+                audioSrc.PlayOneShot(EnemyDamageSound);
+                break;
+            case "Placing":
+                audioSrc.PlayOneShot(PlacingSound);
+                break;
+            case "StartButton":
+                audioSrc.PlayOneShot(StartButtonSound);
+                break;
+        }
+
+    }
+}
